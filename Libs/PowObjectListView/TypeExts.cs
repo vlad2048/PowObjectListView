a72677d9719bb576c4f762156a264e9306afb08f
Vlad Niculescu <vlad.nic@gmail.com>
2023-06-20 01:09:57 +0100

    Initial commit

---
+++ Libs/PowObjectListView/TypeExts.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+
+namespace ObjectListView;
+
+public static class TypeExts
+{
+	public static FieldInfo? GetFieldByMultipleNames(this Type t, BindingFlags bindingFlags, params string[] names)
+	{
+		foreach (var name in names)
+		{
+			var field = t.GetField(name, bindingFlags);
+			if (field != null)
+				return field;
+		}
+		return null;
+	}
+}
